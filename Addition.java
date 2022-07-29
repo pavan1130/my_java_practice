eae8c6d45fbfc4a671c408baf69cb1a8e2d12067
pavan1130 <bpavankumar113011@gmail.com>
2022-07-30 03:51:43 +0530

    added `Addition.java`

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        int b;
+        b = sc.nextInt();
+        int c = a + b;
+        System.out.println("add=" + c);
+    }
+}
